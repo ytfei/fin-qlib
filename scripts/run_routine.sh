55f76936fbc4374d720111707cbe60e0198739da
Mason <mason.p.yang@gmail.com>
2026-02-28 17:39:13 +0800

    fix typo

--- scripts/run_routine.sh
+++ scripts/run_routine.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/zsh
 
 cd /root/codebase/fin-qlib
-uv run python scripts/run_routine.py --config config/online_config.yaml >> data/logs/routine.log 2>&1
+uv run python scripts/run_routine.py --config config/online_config.yaml
+
+# >> data/logs/routine.log 2>&1
